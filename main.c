28f997dd7ffba5a11e39d7602d197432b87db74d
Clayton Carlon <clayton.john.carlon@gmail.com>
2019-04-02 23:05:22 +0000

    The functions rotation(), and upperCase() are tested/

--- main.c
+++ main.c
@@ -6,42 +6,64 @@
 
 //>>>---------> Functions' prototypes
 int length(char *x);
-
+int upperCase(char *x, int n);
+int rotation(char *x, int r, int n);
 
 //>>>---------> Main
 int main()
 {
-    
-    const char trueText[200] = "HELLO, WORLD!";
+    const char trueText[200] = "hELLo, wOrlD!";
     char hiddenText[200];
-    char r = 3; //'r' for rotation
-    char l; //'l' for letter
-    int il; //integer of 'l'
-    int n;
+    int r = 3; //'r' for rotation
+    int n; //length
     
     printf("%s\n", trueText);
     
     strcpy(hiddenText, trueText);
+    
     n = length(hiddenText);
     
-    for (int i = 0; i < n; i++) {
-        l = hiddenText[i];
-        if ((l >= 'A') && (l <= 'Z')) {
-            il = (int)l - 65;
-            il = (il + 3) % 26;
-            l = (char)(il + 65);
-        }  
-        hiddenText[i] = l;
-    }
+    upperCase(hiddenText, n);
+    
+    printf("%s\n", hiddenText);
+    
+    rotation(hiddenText, r, n);
     
     printf("%s", hiddenText);
 }
 
 //>>>---------> Functions' denfinitions
 int length(char *x) {
-    int n = 0;
+    int n = 0; //length
     while (x[n] != 0) {
         n++;
     }
-    return n; //It returns the number of chars including the \0
+    return n; //This returns the number of chars including the \0
+}
+
+int upperCase(char *x, int n) {
+    char l; //'l' for letter
+    for (int i = 0; i < n; i++) {
+        l = x[i];
+        if ((l >= 'a') && (l <= 'z')) {
+            l -= 32;   
+        }  
+        x[i] = l;
+    }  
+    return 1;
+}
+
+int rotation(char *x, int r, int n) {
+    char l; //'l' for letter
+    int il; //integer of 'l'
+    for (int i = 0; i < n; i++) {
+        l = x[i];
+        if ((l >= 'A') && (l <= 'Z')) {
+            il = (int)l - 65;
+            il = (il + r) % 26;
+            l = (char)(il + 65);
+        }  
+        x[i] = l;
+    }    
+    return 1; //This modfies the string by rotation and returns 1 as a flag
 }
