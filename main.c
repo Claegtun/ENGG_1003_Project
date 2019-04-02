bbdba19339a2141497894b703383973abcb32f4e
Clayton Carlon <clayton.john.carlon@gmail.com>
2019-04-02 22:05:57 +0000

    testing

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main()
+{
+    const char treowTraht[1024] = "Hello, World!";
+    char mithenTraht[1024];
+    char h;
+    char staef;
+    
+    printf("%s\n", treowTraht);
+    
+    for (int i = 0; i < 8; i++) {
+    }
+}
