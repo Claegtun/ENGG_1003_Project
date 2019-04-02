af6c29c32a9c2dd160ca6ed26d149568b4d89b0c
Clayton Carlon <clayton.john.carlon@gmail.com>
2019-04-02 23:34:58 +0000

    Encryption and decryption are fully incorporated.

--- main.c
+++ main.c
@@ -5,31 +5,48 @@
 #include<string.h>
 
 //>>>---------> Functions' prototypes
+//One should see definitions for returning flags
 int length(char *x); //returns the length of string 'x'
 int upperCase(char *x, int n); //modifies the lower case char. of string 'x' to upper case and requires length 'n'
 int rotation(char *x, int r, int n); //rotates each alphabetical char. of string 'x' by 'r' and requires length 'n'
 
-//>>>---------> Main
+//>>>---------> Main o-{=========- (sword)
 int main()
 {
-    const char trueText[200] = "hELLo, wOrlD!";
-    char hiddenText[200];
+    char text0[200] = "KHOOR, ZRUOG!";
+    char text1[200];
     int r = 3; //'r' for rotation
     int n; //length
+    int S = 1; 
+    /*S: Action:
+      0  rotational encryption
+      1  rotational decryption */
     
-    printf("%s\n", trueText);
+    //Encrpytion
+    switch(S) {
+        case 0:
+            printf("%s\n", text0);
+            strcpy(text1, text0);
+            n = length(text1);
+            upperCase(text1, n);
+            printf("%s\n", text1);
+            rotation(text1, r, n);
+            printf("%s", text1);   
+            break;
+        case 1:
+            printf("%s\n", text0);
+            strcpy(text1, text0);
+            n = length(text1);
+            upperCase(text1, n);
+            printf("%s\n", text1);
+            rotation(text1, 26 - r, n);
+            printf("%s", text1);
+            break;
+        default:
+            printf("Invalid S");
+    }
     
-    strcpy(hiddenText, trueText);
     
-    n = length(hiddenText);
-    
-    upperCase(hiddenText, n);
-    
-    printf("%s\n", hiddenText);
-    
-    rotation(hiddenText, r, n);
-    
-    printf("%s", hiddenText);
 }
 
 //>>>---------> Functions' denfinitions
