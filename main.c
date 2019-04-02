2ae161185a77478aaae2fbc64d2e36f642bde7ca
Clayton Carlon <clayton.john.carlon@gmail.com>
2019-04-02 23:11:26 +0000

    Additional comments are made.

--- main.c
+++ main.c
@@ -5,9 +5,9 @@
 #include<string.h>
 
 //>>>---------> Functions' prototypes
-int length(char *x);
-int upperCase(char *x, int n);
-int rotation(char *x, int r, int n);
+int length(char *x); //returns the length of string 'x'
+int upperCase(char *x, int n); //modifies the lower case char. of string 'x' to upper case and requires length 'n'
+int rotation(char *x, int r, int n); //rotates each alphabetical char. of string 'x' by 'r' and requires length 'n'
 
 //>>>---------> Main
 int main()
@@ -38,11 +38,12 @@ int length(char *x) {
     while (x[n] != 0) {
         n++;
     }
-    return n; //This returns the number of chars including the \0
+    return n; //returns the number of chars including the \0
 }
 
 int upperCase(char *x, int n) {
     char l; //'l' for letter
+    int f = 0; //'f' for flag
     for (int i = 0; i < n; i++) {
         l = x[i];
         if ((l >= 'a') && (l <= 'z')) {
@@ -50,20 +51,22 @@ int upperCase(char *x, int n) {
         }  
         x[i] = l;
     }  
-    return 1;
+    return f; //returns 1, if rotation occured at all; returns 0 otherwise
 }
 
 int rotation(char *x, int r, int n) {
     char l; //'l' for letter
     int il; //integer of 'l'
+    int f = 0; //'f' for flag
     for (int i = 0; i < n; i++) {
         l = x[i];
         if ((l >= 'A') && (l <= 'Z')) {
             il = (int)l - 65;
             il = (il + r) % 26;
             l = (char)(il + 65);
+            f = 1;
         }  
         x[i] = l;
     }    
-    return 1; //This modfies the string by rotation and returns 1 as a flag
+    return f; //returns 1, if modification occured at all; returns 0 otherwise
 }
