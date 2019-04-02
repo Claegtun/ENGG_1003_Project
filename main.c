b051ccfa40cafb1b8889855b480db477c46462b2
Clayton Carlon <clayton.john.carlon@gmail.com>
2019-04-02 22:49:44 +0000

    Preliminary code for rotation is tested.

--- main.c
+++ main.c
@@ -1,29 +1,47 @@
+//>>>---------> Libraries
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 #include<string.h>
 
-int count(char *x);
+//>>>---------> Functions' prototypes
+int length(char *x);
 
+
+//>>>---------> Main
 int main()
 {
     
     const char trueText[200] = "HELLO, WORLD!";
     char hiddenText[200];
-    char r; //'r' for rotation
+    char r = 3; //'r' for rotation
     char l; //'l' for letter
+    int il; //integer of 'l'
     int n;
     
     printf("%s\n", trueText);
     
     strcpy(hiddenText, trueText);
-    n = count(hiddenText);
+    n = length(hiddenText);
+    
+    for (int i = 0; i < n; i++) {
+        l = hiddenText[i];
+        if ((l >= 'A') && (l <= 'Z')) {
+            il = (int)l - 65;
+            il = (il + 3) % 26;
+            l = (char)(il + 65);
+        }  
+        hiddenText[i] = l;
+    }
+    
+    printf("%s", hiddenText);
 }
 
-int count(char *x) {
+//>>>---------> Functions' denfinitions
+int length(char *x) {
     int n = 0;
     while (x[n] != 0) {
         n++;
     }
-    return n - 1;
+    return n; //It returns the number of chars including the \0
 }
