e3daac736ca6c9e21228c5c4e67f1bac993641f1
Clayton Carlon <clayton.john.carlon@gmail.com>
2019-04-10 01:07:18 +0000

    Rotational decryption without key is tested.

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ int length(char *x); //returns the length of string 'x'
 int upperCase(char *x, int n); //modifies the lower case char. of string 'x' to upper case and requires length 'n'
 int rotation(char *x, int r, int n); //rotates each alphabetical char. of string 'x' by 'r' and requires length 'n'
 int substitution(char *x, char *y, int n); //substitutes an element of string 'x' with the corresponding element of string 'y' and requires length 'n'
+char mostCommon(char *x, int n);
 int decryptingAB(char *x, char *y); //makes an alphabet, i.e. 'y', for decrypting from the encrypting alphabet, i.e. 'x' and requires length 'n'
 
 
@@ -21,8 +22,8 @@ int main()
 {
     //Declaration of variables
     char setting[1]; //the character defining the setting as a header 
-    char text0[1000]; //"SUBMARINES TO DOCK AT ANTWERP AT TWENTY HUNDRED HOURS OVER";
-    char text1[1000] = ""; //the modifying string for the text
+    char word[1000]; //a word from the text
+    char text[1000] = ""; //the text
     char rKey[2]; //the key for rotation; N.B. it must have two digits in the header, e.g. 3 = 03
     char sKey[26] = "QAZXSWEDCVFRTGBNHYUJMKIOLP"; //the key for substitution
     char AB[26]; //the encrytping alphabet
@@ -30,10 +31,11 @@ int main()
     int r; //'r' for rotator, i.e. the integer of the rKey
     int n; //length
     int S; //the setting of the action
+    char M; //Most common letter
     /*S: Action:
       0  rotational encryption
       1  rotational decryption with key 
-      2  rotational decryption without key by frequency of 'T'
+      2  rotational decryption without key by frequency
       3  substitutional encryption with known key
       4  substitutional decryption with known key
     */
@@ -53,74 +55,69 @@ int main()
     
     //Encrpytion
     switch(S) {
+        //Rotational encryption
         case 0:
             fscanf(input, "%2s", rKey);
             r = atoi(rKey);
             printf("%d\n", r);
             while (!feof(input)) {
-                fscanf(input, "%s", text0);
-                strcpy(text1, text0);
-                n = length(text1);
-                upperCase(text1, n);
-                rotation(text1, r, n);
-                printf("%s ", text1);   
+                fscanf(input, "%s", word);
+                n = length(word);
+                upperCase(word, n);
+                rotation(word, r, n);
+                printf("%s ", word);   
             }
             break;
+        //Rotational decryption with key
         case 1:
             fscanf(input, "%2s", rKey);
             r = atoi(rKey);
             printf("%d\n", r);
             while (!feof(input)) {
-                fscanf(input, "%s", text0);
-                strcpy(text1, text0);
-                n = length(text1);
-                upperCase(text1, n);
-                rotation(text1, 26 - r, n);
-                printf("%s ", text1);   
+                fscanf(input, "%s", word);
+                n = length(word);
+                upperCase(word, n);
+                rotation(word, 26 - r, n);
+                printf("%s ", word);   
             }
             break;
+        //Rotational decryption without key by frequency
         case 2:
-            fscanf(input, "%2s", rKey);
-            r = atoi(rKey);
-            printf("%d\n", r);
             while (!feof(input)) {
-                fscanf(input, "%s", text0);
-                n = length(text0);
-                upperCase(text0, n);
-                strcat(text1, text0);
+                fscanf(input, "%s", word);
+                n = length(word);
+                upperCase(word, n);
+                strcat(text, word);
             }
-            printf("%s\n", text1);
+            printf("%s\n", text);
             
-            n = length(text1);
-            char l; //'l' for letter to be tested
-            int f; //'f' for frequency of the letter
-            int m = 0; //'m' for maximum frequency of a letter
-            char M = 'A'; //letter, that has the maxmimum frequency 
-            for (l = 'A'; l <= 'Z'; l++) {
-                f = 0;
-                for (int i = 0; i < n; i++) {
-                    if (text1[i] == l) {
-                        f++;
-                    }        
-                }
-                if (f >= m) {
-                    m = f;
-                    M = l;
-                }
-            }
+            n = length(text);
+            M = mostCommon(text, n);
             printf("%c\n", M);
+            r = (int)(M - 'T');
+            
+            fseek(input, 1, SEEK_SET);
+            
+            while (!feof(input)) {
+                fscanf(input, "%s", word);
+                n = length(word);
+                upperCase(word, n);
+                rotation(word, 26 - r, n);
+                printf("%s ", word);   
+            }
+            
             break;
         case 3: 
             fscanf(input, "%26s", sKey);
             printf("%s\n", sKey);
             strcpy(AB, sKey);
             while (!feof(input)) {
-                fscanf(input, "%s", text0);
-                strcpy(text1, text0);
-                n = length(text1);
-                upperCase(text1, n);
-                substitution(text1, AB, n);    
-                printf("%s ", text1);  
+                fscanf(input, "%s", word);
+                strcpy(text, word);
+                n = length(text);
+                upperCase(text, n);
+                substitution(text, AB, n);    
+                printf("%s ", text);  
             }
             break;
         case 4:
@@ -128,13 +125,13 @@ int main()
             printf("%s\n", sKey);
             strcpy(AB, sKey);
             while (!feof(input)) {
-                fscanf(input, "%s", text0);
-                strcpy(text1, text0);
-                n = length(text1);
-                upperCase(text1, n);
+                fscanf(input, "%s", word);
+                strcpy(text, word);
+                n = length(text);
+                upperCase(text, n);
                 decryptingAB(AB, dAB);
-                substitution(text1, dAB, n);    
-                printf("%s ", text1);  
+                substitution(text, dAB, n);    
+                printf("%s ", text);  
             }
             break;
         default:
@@ -212,6 +209,26 @@ int decryptingAB(char *x, char *y) {
         }
     }
     return f; //returns 1, if any conversion occured at all; returns 0 otherwise
+}
+
+char mostCommon(char *x, int n) {
+    char l; //'l' for letter to be tested
+    int f; //'f' for frequency of the letter
+    int m = 0; //'m' for maximum frequency of a letter
+    char M = 'A'; //letter, that has the maxmimum frequency 
+    for (l = 'A'; l <= 'Z'; l++) {
+        f = 0;
+        for (int i = 0; i < n; i++) {
+            if (x[i] == l) {
+                f++;
+            }        
+        }
+        if (f >= m) {
+            m = f;
+            M = l;
+        }
+    }
+    return M;
 }
 
 
